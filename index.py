727d73764d0db43213a63af16412d82ee233ad3c
Asif Sharif Shahid <asifsharif190@gmail.com>
2024-11-07 15:41:39 +0500

    add multiple convert methods

--- index.py
+++ index.py
@@ -1,10 +1,11 @@
-# Python3 program to convert docx to pdf
-# using docx2pdf module
- 
-# Import the convert method from the
-# docx2pdf module
+# Import the convert method from the docx2pdf module
 from docx2pdf import convert
  
-# Converting docx specifying both the input 
-# and output paths
+# Converting docx present in the same folder
+convert("project_proposal_template.docx")
+ 
+# Converting docx specifying both the input and output paths
 convert("input\project_proposal_template.docx", "output\project_proposal_template.pdf")
+
+# Bulk Conversion
+convert("input\")
